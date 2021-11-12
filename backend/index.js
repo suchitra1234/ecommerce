76f7f59f07f6d455efc08d8a4ddab26b3be96e8d
R Kumar <rkumar@Rs-MacBook-Air.local>
2021-11-12 09:50:47 -0600

    image

--- backend/index.js
+++ backend/index.js
@@ -9,55 +9,72 @@ const products= [
     {
         id:1,
         name:"carrot",
-        price:2
+        price:2,
+        imageurl:"https://source.unsplash.com/random/200x200?carrot"
        
     },
     {
         id:2,
         name:"Cabbage",
-        price:4
+        price:4,
+        imageurl:"https://source.unsplash.com/random/200x200?cabbage"
+
        
     },
     {
         id:3,
         name:"Beets",
-        price:3
+        price:3,
+        imageurl:"https://source.unsplash.com/random/200x200?beetroot"
+
        
     },
     {
         id:4,
         name:"Celery",
-        price:2
+        price:2,
+        imageurl:"https://source.unsplash.com/random/200x200?celery"
+
        
     },
     {
         id:5,
         name:"Egg Plant",
-        price:5
+        price:5,
+        imageurl:"https://source.unsplash.com/random/200x200?eggplant"
+
        
     },
     {
         id:6,
         name:"Okra",
-        price:4
+        price:4,
+        imageurl:"https://source.unsplash.com/random/200x200?okra"
+
        
     },
     {
         id:7,
         name:"Califlower",
-        price:3
+        price:3,
+        imageurl:"https://source.unsplash.com/random/200x200?cauliflower"
+
        
     },
     {
         id:8,
         name:"Beans",
-        price:4
+        price:4,
+        imageurl:"https://source.unsplash.com/random/200x200?beans"
+
        
     },
     {
         id:9,
         name:"Onions",
-        price:5
+        price:5,
+        imageurl:"https://source.unsplash.com/random/200x200?onion"
+
        
     },
 ]
